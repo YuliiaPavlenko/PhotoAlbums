1f619e07c0ce566fd7f548600f26dc340022df9b
Yuliia Pavlenko <>
2020-04-28 13:29:03 +0200

    Create User files structure

---
+++ PhotoAlbums/UI/User/Presenter/UsersListPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  UsersListPresenter.swift
+//  PhotoAlbums
+//
+//  Created by Yuliia Pavlenko on 28/04/2020.
+//  Copyright © 2020 Yuliia Pavlenko. All rights reserved.
+//
+
+import Foundation
