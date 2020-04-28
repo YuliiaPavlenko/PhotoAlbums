e795f408559ce3f169fc57711124fc81ea91bcdf
Yuliia Pavlenko <>
2020-04-28 19:31:11 +0200

    Add common view elements file

--- PhotoAlbums/UI/Common/ViewElements.swift
+++ PhotoAlbums/UI/Common/ViewElements.swift
@@ -6,4 +6,15 @@
 //  Copyright © 2020 Yuliia Pavlenko. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ViewElements {
+
+    static func createUserNameLabel() -> UILabel {
+        let nameLabel = UILabel()
+        nameLabel.textColor = .black
+        nameLabel.textAlignment = .left
+        return nameLabel
+    }
+
+}
