5fd0686a58110afe6452340ce3acbdb1d413923c
Yuliia Pavlenko <>
2020-04-29 18:28:28 +0200

    Implement User List presenter

--- PhotoAlbums/UI/User/Presenter/UsersListPresenter.swift
+++ PhotoAlbums/UI/User/Presenter/UsersListPresenter.swift
@@ -6,4 +6,54 @@
 //  Copyright © 2020 Yuliia Pavlenko. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol UsersListViewDelegate: class {
+    func showUsers(_ users: [UsersListItem])
+//    func showDownloadUserAlbumsDataError(withMessage: DisplayErrorModel)
+    func showUserAlbums()
+    func showProgress()
+    func hideProgress()
+}
+
+
+class UsersListPresenter {
+    var usersList = [UsersListItem]()
+    var originalUsersList = [User]()
+    
+    weak var viewDelegate: UsersListViewDelegate?
+    
+    func viewIsPrepared() {
+        viewDelegate?.showProgress()
+        getUserList()
+    }
+    
+    func onRefreshSwiped() {
+        getUserList()
+    }
+    
+    // MARK: - Get data from server
+    fileprivate func getUserList() {
+        usersList = []
+        
+        NetworkManager.shared.getUsers { [weak self] (users, error) in
+            guard let self = self else { return }
+            
+            self.viewDelegate?.hideProgress()
+            
+            if let users = users {
+
+                for user in users {
+                    let user = UsersListItem(name: user.name)
+                    self.usersList.append(user)
+                }
+                
+                self.viewDelegate?.showUsers(self.usersList)
+            } else {
+                if let error = error {
+
+                }
+            }
+        }
+    }
+}
