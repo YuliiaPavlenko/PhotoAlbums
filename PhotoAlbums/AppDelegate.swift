75d1fb123aa7939f9b147c358bfe5b4e7490c0ce
Yuliia Pavlenko <>
2020-04-29 18:28:58 +0200

    Set user vc as root vc

--- PhotoAlbums/AppDelegate.swift
+++ PhotoAlbums/AppDelegate.swift
@@ -11,11 +11,18 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        let rootViewController = UsersListVC(nibName: nil, bundle: nil)
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+
         return true
     }
-
-
 }
 
