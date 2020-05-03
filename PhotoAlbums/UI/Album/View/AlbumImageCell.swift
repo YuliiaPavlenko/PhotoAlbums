8544233a2e4fb7ffffaf6f841ec9e5ca23f61b3e
Yuliia Pavlenko <>
2020-05-03 19:05:14 +0200

    Implement album-image cells

--- PhotoAlbums/UI/Album/View/AlbumImageCell.swift
+++ PhotoAlbums/UI/Album/View/AlbumImageCell.swift
@@ -6,4 +6,36 @@
 //  Copyright © 2020 Yuliia Pavlenko. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class AlbumImageCell: UITableViewCell {
+    static let Identifier = "AlbumImageCell"
+
+    let albumImageNameLabel = ViewElements.createUserNameLabel()
+    let albumImageView = ViewElements.createAlbumImage()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        addSubviews()
+        configureConstraints()
+    }
+
+    fileprivate func addSubviews() {
+        addSubview(albumImageNameLabel)
+        addSubview(albumImageView)
+    }
+    
+    func configureConstraints() {
+        albumImageNameLabel.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 12, paddingLeft: 25, paddingBottom: 12, paddingRight: 25, width: 0, height: frame.size.height * 0.65, enableInsets: false)
+        albumImageView.anchor(top: topAnchor, left: albumImageNameLabel.rightAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 12, paddingLeft: 25, paddingBottom: 12, paddingRight: 25, width: 0, height: 0, enableInsets: false)
+    }
+    
+    func configureWithUser(album: AlbumItem) {
+        albumImageNameLabel.text = album.albumTitle ?? "No album name"
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- PhotoAlbums/UI/Album/View/ImageAlbumCell.swift
+++ PhotoAlbums/UI/Album/View/ImageAlbumCell.swift
@@ -6,4 +6,13 @@
 //  Copyright © 2020 Yuliia Pavlenko. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ImageAlbumCell: AlbumImageCell {
+    static let Id = "ImageAlbumCell"
+    
+    override func configureConstraints() {
+        albumImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 12, paddingLeft: 25, paddingBottom: 12, paddingRight: 25, width: 0, height: 0, enableInsets: false)
+        albumImageNameLabel.anchor(top: topAnchor, left: albumImageView.rightAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 12, paddingLeft: 25, paddingBottom: 12, paddingRight: 25, width: 0, height: frame.size.height * 0.65, enableInsets: false)
+    }
+}
